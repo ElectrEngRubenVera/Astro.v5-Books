a4c45fea4cdc19590325172200a296401ca5cd64
rubendario4589312 <rubendario4589312@gmail.com>
2025-03-01 03:11:24 +0000

    Book rendering added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,md}", // Add all the folders and files where tailwind css will be used
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    };
