0b369db40930e93e7e78881a49cfaa6b8bafcef2
rubendario4589312 <rubendario4589312@gmail.com>
2025-03-02 11:57:41 +0000

    .

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_Btor2pOK.mjs';
-import { manifest } from './manifest_CC4D-jZk.mjs';
+import { manifest } from './manifest_BpdTfbPQ.mjs';
 
 const serverIslandMap = new Map([
 	['BookScore', () => import('./chunks/BookScore_DlskbF7B.mjs')],
@@ -25,7 +25,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "72add75d-602e-4311-9ccc-b282cfc6ef8a",
+    "middlewareSecret": "1a95cc74-4456-43a8-a907-dc11f9627423",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
